5bc18b2c0af8f34997588a091e0bb806be7b5036
Marius1388 <mariuscristian.bancila@gmail.com>
2023-05-18 20:48:26 +0300

    check width

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 const Nav = () => {
 	return (
 		<nav className="fixed left-0 right-0 top-0 z-20 box-border flex w-screen items-center justify-center bg-black py-3 text-white sm:text-sm md:text-base ">
-			<div className="flex-between flex w-3/5 max-w-3xl items-center justify-center ">
+			<div className="flex-between flex w-3/5 max-w-3xl items-center justify-center max-sm:w-11/12 ">
 				<ScrollLink href="#despre-noi">Despre noi</ScrollLink>
 				<ScrollLink href="#servicii">Servicii</ScrollLink>
 				<ScrollLink href="#galerie">Galerie</ScrollLink>
