84f3e634a5fb170c33a9a15fac8525d0660973fb
Marius1388 <mariuscristian.bancila@gmail.com>
2023-06-07 14:37:06 +0300

    despre noi updated

--- components/DespreNoi.jsx
+++ components/DespreNoi.jsx
@@ -4,16 +4,36 @@ const DespreNoi = () => {
 	return (
 		<section className="flex-center w-full flex-col">
 			<StyledSection propId="despre-noi">
-				<p className="desc mt-10  text-justify indent-10">
-					Traian Vuia a fost un inventator român, pionier al aviației
-					mondiale. În data de 18 martie 1906, a realizat unul din primele
-					zboruri autopropulsate (fără catapulte sau alte mijloace
-					exterioare) cu un aparat mai greu decât aerul.
-				</p>
-				<p className="desc text-justify indent-10 font-semibold">
-					Noi am deschis primul cabinet stomatologic în satul natal al lui
-					Traian Vuia.
-				</p>
+				<section className="flex-center w-full flex-col">
+					<p className="desc mt-10  text-justify indent-10">
+						Absolventă a Facultății de Medicină Dentară din cadrul
+						Universității de Medicină și Farmacie „Victor Babeș” din
+						Timișoara și o experiență de 10 ani în domeniu, dr. Roxana
+						Dancea administrează Cabinetul Stomatologic Traian Vuia, din
+						județul Timiș, înființat în anul 2023, cu ajutorul fondurilor
+						europene din Programul Național de Dezvoltare Rurală” (PNDR).
+						Cabinetul stomatologic din comuna Traian Vuia (localitatea
+						natală a pionierului aviației mondiale, Traian Vuia) asigură
+						servicii de endodonție (tratamente de canal), protetică
+						(lucrări dentare, proteze), estetică și implantologie (prin
+						clinicele partenere) și se adresează atât pacienților din
+						comuna Traian Vuia, cât și pacienților din Țara Făgetului.
+						Prin programul „Smile Diaspora”, venim în sprijinul românilor
+						din diaspora cu un pachet care include tratamente dentare la
+						prețuri decente și consultanță pentru vizitarea celor mai
+						importante obiective din Banat.
+					</p>
+					<p className="desc mt-10  text-justify indent-10">
+						Traian Vuia a fost un inventator român, pionier al aviației
+						mondiale. În data de 18 martie 1906, a realizat unul din
+						primele zboruri autopropulsate (fără catapulte sau alte
+						mijloace exterioare) cu un aparat mai greu decât aerul.
+					</p>
+					<p className="desc text-justify indent-10 font-semibold">
+						Noi am deschis primul cabinet stomatologic în satul natal al
+						lui Traian Vuia.
+					</p>
+				</section>
 			</StyledSection>
 		</section>
 	);
--- components/SmileVillage.jsx
+++ components/SmileVillage.jsx
@@ -4,25 +4,6 @@ const SmileVillage = () => {
 	return (
 		<StyledSection propId="smileVillage">
 			<h2 className="green_gradient text-3xl font-bold ">Smile Diaspora</h2>
-			<section className="flex-center w-full flex-col">
-				<p className="desc mt-10  text-justify indent-10">
-					Absolventă a Facultății de Medicină Dentară din cadrul
-					Universității de Medicină și Farmacie „Victor Babeș” din
-					Timișoara și o experiență de 10 ani în domeniu, dr. Roxana Dancea
-					administrează Cabinetul Stomatologic Traian Vuia, din județul
-					Timiș, înființat în anul 2023, cu ajutorul fondurilor europene
-					din Programul Național de Dezvoltare Rurală” (PNDR). Cabinetul
-					stomatologic din comuna Traian Vuia (localitatea natală a
-					pionierului aviației mondiale, Traian Vuia) asigură servicii de
-					endodonție (tratamente de canal), protetică (lucrări dentare,
-					proteze), estetică și implantologie (prin clinicele partenere) și
-					se adresează atât pacienților din comuna Traian Vuia, cât și
-					pacienților din Țara Făgetului. Prin programul „Smile Diaspora”,
-					venim în sprijinul românilor din diaspora cu un pachet care
-					include tratamente dentare la prețuri decente și consultanță
-					pentru vizitarea celor mai importante obiective din Banat.
-				</p>
-			</section>
 		</StyledSection>
 	);
 };
